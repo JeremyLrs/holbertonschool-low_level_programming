0ac60594c3ad8bf6ca8a25f595664eaee9961631
JeremyLrs <hello@laurensjeremy.com>
2025-06-16 14:14:37 +0200

    Add function puts on my first C script

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - it's my first scrypt im C
+ *
+ * Return: alwways zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
