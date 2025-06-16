1c07824194ffa7a3776eef3f070364600689e5be
JeremyLrs <hello@laurensjeremy.com>
2025-06-16 14:40:13 +0200

    Creat file 5-printf.c for print text Use function prinf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - it's my first project in C
+ *
+ * Return: alwways zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art,")
+	return (0);
+}
