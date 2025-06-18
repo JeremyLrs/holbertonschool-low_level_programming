7776c2cfb7ed0137de342712c11850add67a7054
JeremyLrs <hello@laurensjeremy.com>
2025-06-18 10:44:05 +0200

    Write a program that prints the alphabet in lowercase without letters q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ * Print all the letters except q and e
+ * You can only use the putchar function
+ (every other function (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i == 'q' || i == 'e')
+		{
+			i++;
+			continue;
+		}
+		else
+		{
+			putchar(i);
+
+			i++;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
