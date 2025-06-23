41796de210eaa1e6f7f752ce3d1a02de6f6b4464
JeremyLrs <hello@laurensjeremy.com>
2025-06-23 13:57:15 +0200

    Create script that prints the numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - is function to print the numbers
+ *
+ *
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
