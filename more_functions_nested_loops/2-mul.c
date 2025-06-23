7888d10e07128cce1ceeb04a63af03980a10b737
JeremyLrs <hello@laurensjeremy.com>
2025-06-23 13:13:27 +0200

    Create script thatmultiplies two integers.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - is a function for multiplication
+ *
+ * @a: is an integer
+ * @b: is an integer
+ *
+ * Return: 0
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
