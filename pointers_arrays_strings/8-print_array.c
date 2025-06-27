69181fa8c9648485a82484ada3c6f1e5dbfdde73
JeremyLrs <hello@laurensjeremy.com>
2025-06-27 15:58:45 +0200

    Create script that prints n elements of an array of integers, followed by a new line.

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints n elements
+ * of an array of integers
+ *
+ * @n: aaaa
+ * @a: nnnn
+ *
+ *
+ * Return: 0
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int len;
+
+	for (len = 0; len < n; len++)
+	{
+		printf("%i", a[len]);
+		if (len != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+}
