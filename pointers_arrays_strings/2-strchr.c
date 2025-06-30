20f24e953c8d7cca7ba40bc78a0f5b228687234a
JeremyLrs <hello@laurensjeremy.com>
2025-06-30 16:31:24 +0200

    Create script that locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strchr - function that locates
+ * a character in a string
+ *
+ * @c: character
+ * @s: pointer to the string
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
