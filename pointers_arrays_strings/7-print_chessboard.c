304faf8d8451a38336dd1dff5c05510ae4660c15
JeremyLrs <hello@laurensjeremy.com>
2025-06-30 19:22:54 +0200

    Create script that prints the sum of the two diagonals of a square matrix of intege

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the sum
+ * of the two diagonals of
+ * a square matrix of integers.
+ *
+ *@a: is an array
+ *
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
