9ba850c93bee0e329e75880570a99d1218e13354
JeremyLrs <hello@laurensjeremy.com>
2025-07-02 10:33:18 +0200

    Create script that fills memory with a constant byte

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
 /**
- * *_memset: - a
+ * *_memset - function that fills memory with
+ * a constant byte
  *
- *
- *
- *
+ * @s: is the memory area
+ * @b: is constant byte
+ * @n: is number of first bytes
  *
  * Return: 0
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i = 0;
 
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
