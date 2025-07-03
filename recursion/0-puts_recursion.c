c031b43240f7cd3ae6138409cf56c6592790b2ed
JeremyLrs <hello@laurensjeremy.com>
2025-07-03 11:42:03 +0200

    Create works files for Recursion project

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ *
+ * @s:
+ *
+ *
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+    char i;
+}
