feeeb43a61af7af80aaf8e850e24e61c3b2a229f
JeremyLrs <hello@laurensjeremy.com>
2025-07-07 14:23:03 +0200

    Modification of init char *array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,28 +5,30 @@
  * an array of chars, and initializes it
  * with a specific char.
  *
- * @c:
+ * @size: is a size of array
+ * @c: is a char
  *
  * Return: 0
  */
 
 char *create_array(unsigned int size, char c)
 {
-    int i = 0;
+	unsigned int i = 0;
+	char *array;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    char *array = malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 
-    if (array == NULL)
-    {
-        return (NULL);
-    }
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0 ; i < size ; i++)
-    {
-        array[i] = c;
-    }
-    return (array);
+	for (i = 0 ; i < size ; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
 }
