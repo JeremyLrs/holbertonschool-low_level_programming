73273b91f4d2604c49aac6a0c62f11dcc34690b6
JeremyLrs <hello@laurensjeremy.com>
2025-07-08 15:04:55 +0200

    Modification space for Betty style

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,16 +17,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
 	grid = malloc(sizeof(int *) * height);
-
 	if (grid == NULL)
 		return (NULL);
-
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
-
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
