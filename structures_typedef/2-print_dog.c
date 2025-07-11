5bae39b39448a8c9882478f64d7a3afa8daeef73
JeremyLrs <hello@laurensjeremy.com>
2025-07-11 14:10:41 +0200

    Create script that prints a struct of dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ *
+ * @d: it's a dog
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+
+	printf("Age: %f\n", d->age);
+
+	if (d->name != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("owner: (nil)\n");
+}
