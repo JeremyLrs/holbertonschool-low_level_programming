ef21024ccd9f0135df45dd8386dcd52afe81daf5
JeremyLrs <hello@laurensjeremy.com>
2025-07-11 14:58:07 +0200

    Add {} after the first if for include all the condition after first vericatation

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -5,20 +5,23 @@
  *
  * @d: it's a dog
  *
- * Return: void
+ * Return: 0
  */
 
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
-		printf("Name: %s\n", d->name);
-	else
-		printf("Name: (nil)\n");
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
 
-	printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
-	if (d->name != NULL)
-		printf("Owner: %s\n", d->owner);
-	else
-		printf("owner: (nil)\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("owner: (nil)\n");
+	}
 }
