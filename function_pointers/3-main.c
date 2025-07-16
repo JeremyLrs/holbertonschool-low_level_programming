6a657da4fc4f70664d908af25fbf3a6197157a31
JeremyLrs <hello@laurensjeremy.com>
2025-07-16 15:13:50 +0200

    Create program that performs simple operations.

---
+++ function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - is a main function for basic operations
+ *
+ * @argc: is an argument count
+ * @argv: is an argument value
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (num2 == 0 && (strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0))
+	{
+		printf("Error\n");
+		return (100);
+	}
+	result = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - addition
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+* op_add - subbstraction
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_add - multiplication
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_add - division
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+* op_add - modulo
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
