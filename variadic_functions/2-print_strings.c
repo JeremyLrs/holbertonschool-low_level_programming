53536d3369edde817494f335f3b07f700e8bcffe
JeremyLrs <hello@laurensjeremy.com>
2025-07-18 15:00:27 +0200

    change str != NULL -> str == NULL

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		if (str != NULL)
+		if (str == NULL)
 		{
 			printf("%s", str);
 		}
