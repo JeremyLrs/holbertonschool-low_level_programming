cc869e53979f287e5b01ed16128ba6cd57f60506
JeremyLrs <hello@laurensjeremy.com>
2025-07-18 15:02:22 +0200

    Add () with nil

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,13 +24,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		if (str == NULL)
+		if (str != NULL)
 		{
 			printf("%s", str);
 		}
 		else
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		if (separator != NULL && i < n - 1)
 		{
