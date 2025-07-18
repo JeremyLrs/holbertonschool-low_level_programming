bceb2e1dda39896b4ed9113c4533b326108df8c9
JeremyLrs <hello@laurensjeremy.com>
2025-07-18 10:54:59 +0200

    Delette the librairy from file .h and move the argument in file .c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,11 +11,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
+
 	va_list args;
 
+	va_start(args, n);
 	if (n == 0)
 		return (0);
-	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
